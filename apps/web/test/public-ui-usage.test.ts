f80a8cbff28f5ead7fda2e70eb8d57167dfb2271
gwenphalan <gwen.phalan@gmail.com>
2026-07-24 14:21:13 -0700

    test: include CircuitField export in expected public exports

--- apps/web/test/public-ui-usage.test.ts
+++ apps/web/test/public-ui-usage.test.ts
@@ -49,6 +49,7 @@ describe("public UI package usage", () => {
       'export { Badge } from "./components/ui/badge.js";',
       'export { Button } from "./components/ui/button.js";',
       'export { Card } from "./components/ui/card.js";',
+      'export { CircuitField } from "./components/circuit-field.js";',
       'export { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuLabel, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuSeparator, DropdownMenuTrigger } from "./components/ui/dropdown-menu.js";',
       'export { Kbd, KbdGroup } from "./components/ui/kbd.js";',
       'export { Separator } from "./components/ui/separator.js";',
